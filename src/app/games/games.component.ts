fe93406989b3fa76e0d66fe7ec3b0540b09dd5e6
arikanorh <arikanorh@gmail.com>
2020-04-29 01:15:12 +0300

    1 file modified

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TwitchService } from '../twitch.service';
 
@@ -7,18 +7,27 @@ import { TwitchService } from '../twitch.service';
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.css']
 })
-export class GamesComponent implements OnInit {
+export class GamesComponent implements OnInit,OnDestroy {
   games;
+  listener;
 
   constructor(private twitch:TwitchService){
-
+ 
   }
 
   ngOnInit(){
     this.twitch.getGames().subscribe(
       e=>{
-        this.games = e.top;
+        this.games = e.top; 
       }
     );
+
+    this.listener = document.addEventListener('keydown',(e:KeyboardEvent)=>{
+      console.log(e.key);
+    });
+  }
+
+  ngOnDestroy(){
+    document.removeEventListener('keypress',this.listener);
   }
 }
