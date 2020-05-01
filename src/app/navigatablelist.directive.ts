e9d75b6104f3450f7e4e5dd5c7f6113cb830c546
Orhan ARIKAN <arikanorh@gmail.com>
2020-05-01 20:38:48 +0300

    Nav düzenleme

--- src/app/navigatablelist.directive.ts
+++ src/app/navigatablelist.directive.ts
@@ -1,60 +1,95 @@
-import { Directive, ElementRef, ContentChildren, AfterViewInit, OnInit, QueryList } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  ContentChildren,
+  AfterViewInit,
+  OnInit,
+  QueryList,
+  AfterContentChecked,
+  OnDestroy
+} from "@angular/core";
+import { Observable, Subscription } from "rxjs";
 
 @Directive({
-  selector: '[appNavigatablelist]'
+  selector: "[appNavigatablelist]"
 })
-export class NavigatablelistDirective implements  AfterViewInit {
-
+export class NavigatablelistDirective
+  implements AfterViewInit, AfterContentChecked, OnDestroy {
   @ContentChildren("nav", { read: ElementRef }) channels: QueryList<ElementRef>;
   focusIndex = 0;
   listener;
+  sub: Subscription;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
+  ngAfterContentChecked() {
+    try {
+      if (!this.hasCurrentFocus()) {
+        this.focusIndex = 0;
+        this.focusCurrentElement();
+      }
+    } catch (err) {}
+  }
 
   ngAfterViewInit(): void {
-
-    this.channels.changes.subscribe(items => {
-
-      let array = items.toArray();
-      for (let index = 0; index < array.length; index++) {
-        let element = array[index];
-        element.nativeElement.setAttribute("tabindex", index);
-      }
-      this.focusCurrentElement();
-    })
+    let array = this.channels.toArray();
+    if (array.length !== 0) {
+      this.setTabIndexesAndFocus(array);
+    } else
+      this.sub = this.channels.changes.subscribe(items => {
+        let array = items.toArray();
+        this.setTabIndexesAndFocus(array);
+      });
 
     this.listener = document.addEventListener("keydown", (e: KeyboardEvent) => {
-       
-      if (e.key === "ArrowRight") {
+      if (!this.hasCurrentFocus()) {
+        this.focusIndex = 0;
+        this.focusCurrentElement();
+        return;
+      }
 
+      if (e.key === "ArrowRight") {
         this.focusIndex++;
-        this.focusIndex = this.focusIndex >= this.channels.length ? 0 : this.focusIndex;
+        this.focusIndex =
+          this.focusIndex >= this.channels.length ? 0 : this.focusIndex;
         this.focusCurrentElement();
-      }
-      else if (e.key === "ArrowLeft") {
+      } else if (e.key === "ArrowLeft") {
         this.focusIndex--;
-        this.focusIndex = this.focusIndex < 0 ? this.channels.length - 1 : this.focusIndex;
+        this.focusIndex =
+          this.focusIndex < 0 ? this.channels.length - 1 : this.focusIndex;
         this.focusCurrentElement();
-      }
-      else if (e.key === "Enter") {
-         this.channels.toArray()[this.focusIndex].nativeElement.click();
+      } else if (e.key === "Enter") {
+        this.channels.toArray()[this.focusIndex].nativeElement.click();
       }
     });
-
   }
   ngOnDestroy() {
     document.removeEventListener("keypress", this.listener);
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
-
   focusCurrentElement() {
-    if (this.focusIndex < 0)
-      this.focusIndex = this.channels.length - 1;
-    else if (this.focusIndex >= this.channels.length)
+    if (this.focusIndex < 0) this.focusIndex = this.channels.length - 1;
+    else if (this.focusIndex >= this.channels.length) {
       this.focusIndex = 0;
+    }
     let element = this.channels.toArray()[this.focusIndex].nativeElement;
     element.focus();
   }
 
+  hasCurrentFocus() {
+    let elem = this.channels.toArray()[this.focusIndex].nativeElement;
+    return elem === document.activeElement;
+  }
+
+  private setTabIndexesAndFocus(elements: ElementRef[]) {
+    for (let index = 0; index < elements.length; index++) {
+      let element = elements[index];
+      element.nativeElement.setAttribute("tabindex", index);
+    }
+    this.focusIndex = 0;
+    this.focusCurrentElement();
+  }
 }
