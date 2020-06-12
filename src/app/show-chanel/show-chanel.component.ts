d213fb2040fee2989906fb7c53263af9246d0ddb
arikanorh <arikanorh@gmail.com>
2020-06-12 20:25:37 +0300

    1 file modified

--- src/app/show-chanel/show-chanel.component.ts
+++ src/app/show-chanel/show-chanel.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-
-} from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { DomSanitizer } from "@angular/platform-browser";
 import { TwitchService } from "../twitch.service";
@@ -18,7 +12,7 @@ export class ShowChanelComponent implements OnInit {
   @ViewChild("wrapper") video: ElementRef;
   data$;
   url;
-   showList: boolean = false;
+  showList: boolean = false;
   constructor(
     private route: ActivatedRoute,
     private sanitizer: DomSanitizer,
@@ -29,8 +23,7 @@ export class ShowChanelComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.url = this.getUrl(params.id);
     });
- 
-    
+
     window.addEventListener("keydown", this.eventListener);
     this.data$ = this.twitch.getFavStreams();
   }
@@ -40,33 +33,41 @@ export class ShowChanelComponent implements OnInit {
   }
 
   eventListener = (e: KeyboardEvent) => {
-      if (e.key === "ArrowUp") {
-        if (!this.showList) {
-          this.video.nativeElement.webkitRequestFullScreen();
-        }
-      } else if (e.key === "ArrowRight") {
-        this.toggleShow();
-        // this.modal.hide();
-      } else if (e.key === "ArrowLeft") {
-        this.toggleShow();
-      } else if (e.key === "Enter") {
-        this.showList = false;
-      } else if (e.key === "ArrowDown") {
+    if (e.key === "ArrowUp") {
+      if (!this.showList) {
+        this.video.nativeElement.webkitRequestFullScreen();
       }
-    };
+    } else if (e.key === "ArrowRight") {
+      this.toggleShow();
+      // this.modal.hide();
+    } else if (e.key === "ArrowLeft") {
+      this.toggleShow();
+    } else if (e.key === "Enter") {
+      this.showList = false;
+    } else if (e.key === "ArrowDown") {
+    }
+  };
 
   getUrl(id: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(
-      "https://player.twitch.tv/?channel=" + id + "&html5=1"
+      "https://player.twitch.tv/?channel=" +
+        id +
+        "&html5=1&parent="+this.getBaseUrl()
     );
   }
 
   toggleShow() {
     let currentShow = this.showList;
     let nextShow = !currentShow;
-    if(nextShow){
+    if (nextShow) {
       this.video.nativeElement.focus();
     }
     this.showList = nextShow;
+  }
+
+  getBaseUrl() {
+    return window.location.origin
+      .replace("https://", "")
+      .replace("http://", "");
   }
 }
