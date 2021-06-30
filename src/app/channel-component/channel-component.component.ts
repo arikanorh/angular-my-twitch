fe8ecf5b2dcafad74534f55a90275307cc34678c
arikanorh <arikanorh@gmail.com>
2021-06-30 21:04:13 +0300

    3 files modified

--- src/app/channel-component/channel-component.component.ts
+++ src/app/channel-component/channel-component.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, HostListener } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
 @Component({
@@ -17,33 +16,24 @@ export class ChannelComponentComponent implements OnInit {
   viewers;
   game_name;
 
-  constructor(private _sanitizer: DomSanitizer, private router: Router) {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    //console.log(this.value);
-    this.channel_name = this.value.user_name; //user_name
-    this.channel_id = this.value.user_login; //user_login
     this.channel_image = this.value.thumbnail_url
       .replace('{width}', '320')
       .replace('{height}', '180'); // thumbnail_url - replace height-width
-    this.channgel_display_name = this.value.user_name; // user_name
-    this.channel_status = this.value.title; // title
-    this.viewers = this.value.viewer_count; // viewer_count
-    this.game_name = this.value.game_name; // game_name
   }
-  getBackground() {
-    return this._sanitizer.bypassSecurityTrustStyle(
-      `linear-gradient(rgba(29, 29, 29, 0), rgba(16, 16, 23, 0.5)), url(${
-        this.channel_image
-      })`
-    );
-  }
+
+
+
   @HostListener('click')
   handleClick() {
-    this.router.navigate(['show', this.channel_name]); //user_login
+    this.router.navigate(['show', this.value.user_login]); //user_login
   }
   goMobile() {
-    window.location.href =
-      'https://m.twitch.tv/' + this.channel_name + '?no-mobile-redirect=true';
+    window.location.href = 
+      'https://m.twitch.tv/' +
+      this.value.user_login +
+      '?no-mobile-redirect=true';
   }
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TwitchService } from '../twitch.service';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
+export class TestComponent {
   href;
 
-  constructor() {}
-
-  ngOnInit() {
-    this.href = 'https://id.twitch.tv/oauth2/authorize?';
-    this.href += 'client_id=1e4gz76ye3w3f71ic955m4seb8jfph';
-    this.href += '&';
-    this.href += 'redirect_uri=' + encodeURIComponent(this.getBaseUrl());
-    this.href += '&';
-    this.href += 'response_type=token';
-    this.href += '&';
-    this.href += 'scope=user:read:follows';
+  constructor(private twitch: TwitchService) {
+    this.href = twitch.getOauthUrl();
   }
 
   getBaseUrl() {
