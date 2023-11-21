e4a2307e5458041cabbb2edfcc29da0f34d5752b
arikanorh <arikanorh@gmail.com>
2023-11-21 14:34:27 +0300

    Import fix

--- src/app/show-chanel/tw-embed/tw-embed.component.ts
+++ src/app/show-chanel/tw-embed/tw-embed.component.ts
@@ -1,21 +1,29 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DebugService } from 'src/app/devthings/debug-service.service';
-import { VideoState } from 'src/app/model/VideoStates';
-import Twitch from 'src/assets/scripts/twitch.js';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { DebugService } from '../../devthings/debug-service.service';
+import { VideoState } from '../../model/VideoStates';
+import Twitch from '../../../assets/scripts/twitch.js';
 
 @Component({
   selector: 'app-tw-embed',
   templateUrl: './tw-embed.component.html',
   styleUrls: ['./tw-embed.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TwEmbedComponent implements OnInit {
-
   player;
   _id;
   @Input() set id(value: string) {
     if (this.player) {
-      this.debugService.addLog("Updating channel from " + this._id + " to " + value);
+      this.debugService.addLog(
+        'Updating channel from ' + this._id + ' to ' + value
+      );
       this.player.setChannel(value);
     }
     this._id = value;
@@ -24,38 +32,39 @@ export class TwEmbedComponent implements OnInit {
   embed;
   @Output() videostatechange = new EventEmitter<any>();
 
-
-
-  constructor(private debugService: DebugService) {
-  }
-
+  constructor(private debugService: DebugService) {}
 
   ngOnInit() {
     let self = this;
 
-    let embed = new Twitch.Player("twitch-embed", {
-      width: "100%",
-      height: "100%",
+    let embed = new Twitch.Player('twitch-embed', {
+      width: '100%',
+      height: '100%',
       channel: this._id,
       autoplay: true,
       // Only needed if this page is going to be embedded on other websites
-      parent: [this.getBaseUrl()]
+      parent: [this.getBaseUrl()],
     });
     this.player = embed.getPlayer();
 
     embed.addEventListener(Twitch.Embed.VIDEO_READY, function () {
-      self.debugService.addLog("Settings volume to 100%");
+      self.debugService.addLog('Settings volume to 100%');
       self.player.setVolume(1);
-      self.videostatechange.emit({ id: self._id, type: VideoState.VIDEO_READY });
+      self.videostatechange.emit({
+        id: self._id,
+        type: VideoState.VIDEO_READY,
+      });
     });
-
 
     embed.addEventListener(Twitch.Embed.VIDEO_PLAY, function (e) {
       self.videostatechange.emit({ id: self._id, type: VideoState.VIDEO_PLAY });
     });
 
     embed.addEventListener(Twitch.Embed.VIDEO_PAUSE, function () {
-      self.videostatechange.emit({ id: self._id, type: VideoState.VIDEO_PAUSE });
+      self.videostatechange.emit({
+        id: self._id,
+        type: VideoState.VIDEO_PAUSE,
+      });
     });
 
     this.embed = embed;
@@ -69,7 +78,6 @@ export class TwEmbedComponent implements OnInit {
     this.player.play();
   }
 
-
   getBaseUrl() {
     return window.location.hostname;
   }
@@ -81,6 +89,4 @@ export class TwEmbedComponent implements OnInit {
   setVolume(vol) {
     this.player.setVolume(vol);
   }
-
-
 }
